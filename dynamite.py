19620395fff788b521c1f44a8dfb535b42205abd
Tanmay Adithya <marnitanmayadithya@gmail.com>
2024-02-17 01:10:28 +0530

    feat: Parser enters the chat

--- dynamite.py
+++ dynamite.py
@@ -1,15 +1,21 @@
 from lex import *
+from parse import *
+import sys
 
 
 def main():
-    source = "IF+-123 foo*THEN/"
-    lex = Lexer(source)
+    print("Dynamite Compiler")
 
-    token = lex.getToken()
+    if len(sys.argv) != 2:
+        sys.exit("Error: Compiler needs source file as argument.")
+    with open(sys.argv[1], 'r') as inputFile:
+        source = inputFile.read()
 
-    while token.kind != TokenType.EOF:
-        print(token.kind)
-        token = lex.getToken()
+    lexer = Lexer(source)
+    parser = Parser(lexer)
+
+    parser.program()
+    print("Parsing completed.")
 
 
 main()
